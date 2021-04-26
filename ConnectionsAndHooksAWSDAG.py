494963744e9849909f9d4c862853b49223924fac
Brad Lensing <bradlensing@yahoo.com>
2021-04-25 21:45:19 -0500

    Set up the prefix variable

--- ConnectionsAndHooksAWSDAG.py
+++ ConnectionsAndHooksAWSDAG.py
@@ -7,6 +7,7 @@ from airflow.operators.python import PythonOperator
 from airflow.hooks.S3_hook import S3Hook
 
 # CREATE AWS VARIABLES
+# S3 Path = s3://udacity-dend/udac-data-pipelines/divvy/ 
 # airfow console > Admin > Variables > + New One
     # Key = "s3_bucket" Val = "udacity-dend"
     # Key = "s3_prefix" Val = "data-pipelines"
@@ -18,14 +19,16 @@ from airflow.hooks.S3_hook import S3Hook
 def list_keys():
     hook = S3Hook(aws_conn_id='aws_credentials')
     bucket = Variable.get('s3_bucket')
-    logging.info(f"Listing Keys from {bucket}")
-    keys = hook.list_keys(bucket)
+    prefix = Variable.get('s3_prefix')
+    logging.info(f"Listing Keys from {bucket}/{prefix}")
+    keys = hook.list_keys(bucket,prefix=prefix)
     for key in keys:
         logging.info(f" - s3://{bucket}/{key}")
 
 dag = DAG(
     dag_id='connections_hooks_aws',
     start_date=datetime.datetime.now(),
+    catchup=False,
     schedule_interval=None,
     tags=['demo']
 )
@@ -34,5 +37,4 @@ list_task = PythonOperator(
     task_id='list_task',
     python_callable=list_keys,
     dag=dag
-)
-
+    )
